3c448ed5e2b167c397289ad5980542cb2e73b65a
Rakibul-98 <rakibul.rupom2001@gmail.com>
2025-07-11 11:12:03 +0600

    search functionality added

--- src/app/components/Products.tsx
+++ src/app/components/Products.tsx
@@ -1,4 +1,8 @@
+"use client";
+
 import Link from "next/link";
+import { useState } from "react";
+import { FaSearch } from "react-icons/fa";
 
 interface Product {
   id: number;
@@ -60,18 +64,36 @@ export const getProductById = (id: number) =>
   dummyProducts.find((p) => p.id === id);
 
 export default function ProductGrid({ category }: { category: string }) {
-  const products = getProductsByCategory(category);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const filteredProducts = getProductsByCategory(category).filter((product) =>
+    product.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
     <div className="flex-1 px-4 py-6">
-      <h2 className="text-2xl font-bold mb-6">{category}</h2>
-      {products.length === 0 ? (
-        <p className="text-gray-500">
-          No products available for this category.
-        </p>
+      <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center gap-4 mb-6">
+        <h2 className="text-2xl font-bold">{category}</h2>
+
+        <div className="relative">
+          <span className="absolute top-2 left-2">
+            <FaSearch />
+          </span>
+          <input
+            type="text"
+            placeholder="Search here..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className="border border-gray-300 rounded px-3 ps-7.5 py-1 w-full sm:w-68 focus:outline-none focus:border-black"
+          />
+        </div>
+      </div>
+
+      {filteredProducts.length === 0 ? (
+        <p className="text-gray-500">No products found.</p>
       ) : (
         <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
-          {products.map((product) => (
+          {filteredProducts.map((product) => (
             <Link key={product.id} href={`/${product.id}`}>
               <div className="border rounded-md p-2 shadow-sm hover:shadow-md transition cursor-pointer">
                 <div className="h-32 bg-blue-100 rounded mb-2"></div>
